1e881660040fa37679fffe0cbf2658e4167a761b
Brandt Green <153144Green@chsbr.net>
2020-07-15 23:20:38 -0500

    Initial commit with basic blog infastructure.

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 
 
-class PublishedManager(models.Manager):
+class CreatedManager(models.Manager):
     """custom manager. I think it returns a query that is automatically filtered for Post that are created... which is all of them"""
     # This is probably pointless right?
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='created')
+    #def get_queryset(self):
+    #    return super(CreatedManager, self).get_queryset().filter(status='created')
 
 class Post(models.Model):
     """Class to model a blog post. """
@@ -23,10 +24,10 @@ class Post(models.Model):
     updated = models.DateField(auto_now=True)
     #status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-
+        
 
     objects = models.Manager()
-    published = CreatedManager() # This is probably pointless right?
+    #created_posts = CreatedManager() # This is probably pointless right?
 
     class Meta:
         """Sort posts by created date"""
@@ -37,3 +38,12 @@ class Post(models.Model):
         """This is what shows up in admin page"""
         return self.title
 
+
+    def get_absolute_url(self):
+        """Retrieve the absolute url for a post"""
+        # I do not understand how this works.
+        return reverse('blog:post_detail',
+                       args=[self.created.year,
+                             self.created.month,
+                             self.created.day,
+                             self.slug])
--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request, tag_slug=None):
+    """Displays the posts."""
+
+    #post = Post.created_posts.all()
+    posts = Post.objects.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+    #object_list = Post.published.all()
+    #tag = None
+
+    #if tag_slug:
+    #    tag = get_object_or_404(Tag, slug=tag_slug)
+    #    object_list = object_list.filter(tags__in=[tag])
+
+    #paginator = Paginator(object_list, 3) # 3 posts per page
+    #page = request.GET.get('page')
+    #try:
+    #    posts = paginator.page(page)
+    #except PageNotAnInteger:
+    #    posts = paginator.page(1)
+    #except EmptyPage:
+    #    posts = paginator.page(paginator.num_pages)
+
+    #return render(request, 'blog/post/list.html', {'page': page,'posts': posts, 'tag': tag})
+
+
+def post_detail(request, year, month, day, post):
+    """Single Post"""
+
+    post = get_object_or_404(Post, slug=post,                                  
+                                   publish__year=year, 
+                                   publish__month=month,
+                                   publish__day=day)
+
+
+    return render(request, 'blog/post/detail.html',
+                 {'post': post,})
